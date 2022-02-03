639152b727f4ad42b8a5799b3af1f76d0cf6445f
616 <32841294+Dofamin@users.noreply.github.com>
2022-02-04 02:05:41 +0300

    Create entrypoint.sh

---
+++ container-image-root/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+
+if [ ! -f /Dante/sockd.conf ]; then
+    cp /sockd.conf /Dante/sockd.conf
+fi
+
+if [ ! -f /Dante/Users.list.txt ]; then
+    cp /Users.list.txt /Dante/Users.list.txt
+fi
+
+if [ ! -f /Dante/Users.list.txt ]; then
+    cp /Users.list.txt /Dante/Users.list.txt
+fi
+
+function update_users() {
+    while read -r line; do
+        User=$(echo "$line" | awk -F":" '{print $1}')
+        deluser "$User" >/dev/null 2>&1
+        echo "$User deleted"
+        delgroup "$User" >/dev/null 2>&1
+    done </Dante/Users.list.txt
+    while read -r line; do
+        User=$(echo "$line" | awk -F":" '{print $1}')
+        Password=$(echo "$line" | awk -F":" '{print $2}')
+        useradd "$User" -M -s /sbin/nologin
+        echo "${User}:$Password" | chpasswd
+        echo "$User added"
+    done </Dante/Users.list.txt
+}
+
+echo "Updating users."
+update_users
+service ntp start
+service cron start
+/usr/sbin/danted -f /Dante/sockd.conf
